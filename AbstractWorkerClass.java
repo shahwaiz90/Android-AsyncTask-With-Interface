36011e70431dd769903edf56c5fb9ca46f9be562
Ahmad Shahwaiz <shahwaiz90@yahoo.com>
2018-10-31 17:29:25 +0500

    Create AbstractWorkerClass.java

---
+++ AbstractWorkerClass.java
@@ -0,0 +1,8 @@
+import android.support.v7.app.AppCompatActivity;
+
+
+abstract class AbstractWorkerClass extends AppCompatActivity{
+    protected void logoutScenario(){
+        finish();
+    }
+}
